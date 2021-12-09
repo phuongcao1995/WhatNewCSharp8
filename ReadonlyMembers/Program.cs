05de04d0a78731c6b296e89f2e4f96ea2109c47e
Phuong Cao <caongocphuong95@gmail.com>
2021-12-09 11:18:13 +0700

    update 3

--- ReadonlyMembers/Program.cs
+++ ReadonlyMembers/Program.cs
@@ -6,14 +6,14 @@ namespace ReadonlyMembers
     {
         public double Height { get; set; }
         public double Width { get; set; }
-        public double Area => (Height * Width);
+        public readonly double Area => (Height * Width);
 
-        //public void setHeight(int height)
-        //{
-        //    Height = height;
-        //}
+        public void setHeight(int height)
+        {
+            Height = height;
+        }
 
-        public override string ToString()
+        public  override readonly string ToString()
         {
             return $"Total area: {Area}";
         }
