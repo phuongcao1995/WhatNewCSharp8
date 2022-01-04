ce855bd23cd3996a592b553a81bd7ae374bcc865
Phuong Cao <caongocphuong95@gmail.com>
2022-01-04 14:19:39 +0700

    part 2

---
+++ StaticLocalFunctions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StaticLocalFunctions
+{
+    class Program
+    {
+        static int M()
+        {
+            int y;
+            LocalFunction();
+            return y;
+
+            void LocalFunction() => y = 0;
+        }
+
+        static void CheckNumber(int x)
+        {
+            DoSomeThing();
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            void DoSomeThing()
+            {
+                x++;
+                //do something
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 2;
+            CheckNumber(number);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UsingDeclarations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UsingDeclarations
+{
+    class Program
+    {
+        static void ReadFile(string path)
+        {
+
+            var file = new ExtendStreamReader(path);
+            Console.WriteLine(file.ReadToEnd());
+
+
+            //using (var file = new ExtendStreamReader(path))
+            //{
+            //    Console.WriteLine(file.ReadToEnd());
+            //}
+
+
+            //using var file = new ExtendStreamReader(path);
+            //Console.WriteLine(file.ReadToEnd());
+        }
+
+        static void Main(string[] args)
+        {
+            var path = @"D:\project\FPT_SharingTech\WhatNewCSharp8\UsingDeclarations\TestFile.txt";
+            ReadFile(path);
+
+            Console.ReadLine();
+        }
+    }
+}
